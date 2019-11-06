944dcbd16978b8678719859b229ac9fee2e4fffa
taufiq.ivan15@student.untan.ac.id <taufiq.ivan15@student.untan.ac.id>
2019-11-06 19:53:56 +0700

    update

--- app/src/main/java/com/example/tbtekber/CariEventActivity.java
+++ app/src/main/java/com/example/tbtekber/CariEventActivity.java
@@ -3,6 +3,7 @@ package com.example.tbtekber;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class CariEventActivity extends AppCompatActivity {
 
@@ -10,5 +11,23 @@ public class CariEventActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cari_event);
+    }
+
+    public void event1(View view) {
+    }
+
+    public void event2(View view) {
+    }
+
+    public void event3(View view) {
+    }
+
+    public void event4(View view) {
+    }
+
+    public void event5(View view) {
+    }
+
+    public void event6(View view) {
     }
 }
