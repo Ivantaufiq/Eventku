d542a64f09b123f7d78c613f2268b0358d57120f
taufiq.ivan15@student.untan.ac.id <taufiq.ivan15@student.untan.ac.id>
2019-11-07 14:11:01 +0700

    update

---
+++ app/src/main/java/com/example/tbtekber/UbahEmail.java
@@ -0,0 +1,14 @@
+package com.example.tbtekber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UbahEmail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ubah_email);
+    }
+}
