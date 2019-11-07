38d72bf0783b6f534cb513ccd7d2a4edad3dcc13
taufiq.ivan15@student.untan.ac.id <taufiq.ivan15@student.untan.ac.id>
2019-11-07 13:23:56 +0700

    update

---
+++ app/src/main/java/com/example/tbtekber/ProfiActivity.java
@@ -0,0 +1,14 @@
+package com.example.tbtekber;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ProfiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profi);
+    }
+}
